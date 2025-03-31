c62d6ab7f878f0100d35d5fe98b6a0a1dda2ccd7
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-03-31 17:30:44 +0300

    initializing navigation and Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Header from './components/header/Header';
+import Navigation from './components/navigation/Navigation';
 import '/public/styles/styles.css';
 
 function App() {
   return (
     <>
-  <h1>Hello</h1>
+  <Navigation />
+  <Header />
      </>
   )
 }
