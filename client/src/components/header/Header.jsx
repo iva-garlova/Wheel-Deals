dde2b213995b8522b5d673c8e1b7b34b0cf45724
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-03-31 18:54:43 +0300

    implementing the main page (navigation and header)

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router";
+
 export default function Header() {
     return (
         <header className="masthead">
         <div className="container">
-            <div className="masthead-subheading">Welcome To Our Studio!</div>
-            <div className="masthead-heading text-uppercase">Its Nice To Meet You</div>
-            <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+            <div className="masthead-subheading">Welcome To Wheel Deals!</div>
+            <div className="masthead-heading text-uppercase">Its Nice To Have You</div>
+            <Link className="btn btn-primary btn-xl text-uppercase" to="/about">About us</Link>
         </div>
     </header>
     );
--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,19 +1,24 @@
+import {Link} from 'react-router'
+
 export default function Navigation() {
     return (
+    
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div className="container">
-            <a className="navbar-brand" href="#page-top"><img src="/img/navbar-logo.svg" alt="..." /></a>
+        <Link className="navbar-brand" to="#page-top">
+  <img src="/img/navbar-logo.svg" alt="..." />
+       </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i className="fas fa-bars ms-1"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                    <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#team">Team</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                    <li className="nav-item"><Link className="nav-link" to="/home">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="#contact">Register</Link></li>
                 </ul>
             </div>
         </div>
