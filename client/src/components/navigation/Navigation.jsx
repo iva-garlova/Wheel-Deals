c4cc91625300aa543e683bbbb469cdeaf89fb4b5
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-04-01 19:43:15 +0300

    setting logo

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -6,7 +6,7 @@ export default function Navigation() {
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div className="container">
         <Link className="navbar-brand" to="#page-top">
-  <img src="/img/navbar-logo.svg" alt="Logo" />
+  {/* <img src="/img/horizontal.svg" alt="Logo" /> */}
        </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
@@ -14,10 +14,12 @@ export default function Navigation() {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                    <li className="nav-item"><Link className="nav-link" to="/home">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/products/create">Create</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="/logout">Logout</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="#contact">Register</Link></li>
                 </ul>
             </div>
