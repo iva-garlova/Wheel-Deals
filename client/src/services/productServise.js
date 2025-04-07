51f9d597c194b3a2ddbc7a570d41dc2c1bdc9de7
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-04-07 10:53:06 +0300

    fixing returning of Object.values

--- client/src/services/productServise.js
+++ client/src/services/productServise.js
@@ -5,9 +5,9 @@ const baseUrl = 'http://localhost:3030/jsonstore/products';
 export default {
     async getAll(){
         const result = await request.get(baseUrl);
-        const products = Object.values(result);
+        // const products = Object.values(result);
 
-        return products;
+        return result ? Object.values(result) : [];
     },
 
     getOne(productId){
